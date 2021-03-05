f94220e7f91985de6b51b656a8e3bb41555c31d1
Ian Rose <ianrose@ians-mbp-2.student.wheaton.edu>
2021-03-04 23:01:35 -0600

    first look at what our databse will look like -ish

--- api/models.py
+++ api/models.py
@@ -1,6 +1,15 @@
 from . import db
+from datetime import datetime
 
 class Drive(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    rating = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True) #synonymous with drive
+    trip = db.Column(db.Integer)
+    start = db.Column(db.DateTime)
+    restaurant_arrival = db.Column(db.DateTime)
+    restaurant_leave = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+    restaurant = db.Column(db.String(75))
+    pay = db.Column(db.Float)
+    delivery_time = db.Column(db.Float)
+    restaurant_time = db.Column(db.Float)
+    distance = db.Column(db.Float)
--- api/views.py
+++ api/views.py
@@ -8,25 +8,33 @@ main = Blueprint('main', __name__)
 def home():
     return jsonify(data="Landing page so it's not an error")
 
-@main.route('/add_movie', methods=["POST"])
-def add_movie():
+@main.route('/add_drive', methods=["POST"])
+def add_drive():
 
-    movie_data = request.get_json()
+    drive_data = request.get_json()
 
-    new_drive = Drive(title=movie_data['title'], rating=movie_data['rating'])
+    new_drive = Drive(distance=drive_data['distance'], pay=drive_data['pay'], restaurant=drive_data['restaurant'])
 
     db.session.add(new_drive)
     db.session.commit()
     
     return "Done", 201
 
-@main.route('/movies')
-def movies():
+@main.route('/drives')
+def drives():
 
-    movie_list = Drive.query.all()
-    movies = []
+    drive_list = Drive.query.all()
+    drives = []
 
-    for movie in movie_list:
-        movies.append({'title' : movie.title, 'rating' : movie.rating})
+    for drive in drive_list:
+        drives.append({'restaurant' : drive.restaurant, 'distance' : drive.distance, 'pay' : drive.pay})
 
-    return jsonify({'movies': movies})
+    return jsonify({'drives': drives}), 201
+
+@main.route('/remove')
+def remove():
+
+    deleted = Drive.query.delete()
+    db.session.commit()
+
+    return str(deleted) + " deleted", 201
