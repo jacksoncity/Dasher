b29d4b797d06823181e3cd49965626d1c8470198
Andrea Tyson <andreatyson10@gmail.com>
2021-03-07 14:12:31 -0600

    Basic navigation implemented

--- dasher-app/App.js
+++ dasher-app/App.js
@@ -1,13 +1,51 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import React from 'react';
+import * as React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function LoginScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+      <Button
+        title="Go to Main"
+        onPress={() => navigation.navigate('Main')}
+      />
+    </View>
+  );
+}
+
+function MainScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Main Screen</Text>
+      <Button
+        title="Go to Main... again"
+        onPress={() => navigation.navigate('Main')}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+	<NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Main" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    /*<View style={styles.container}>
       <Text>This will be the Dasher app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>*/
   );
 }
 
