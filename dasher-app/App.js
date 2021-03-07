509d7e1a9c0b048e3f2cf903014e8e602c496ddf
Andrea Tyson <andreatyson10@gmail.com>
2021-03-07 14:50:11 -0600

    Login screen configured

--- dasher-app/App.js
+++ dasher-app/App.js
@@ -1,21 +1,44 @@
 import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
+//import * as React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
 
 //Login screen
 function LoginScreen({ navigation }) {
+  //const [username, setUsername] = useState("");
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen</Text>
-      <Button
-        title="Go to Main"
-        onPress={() => navigation.navigate('Main')}
+      <Text style={styles.title}> 
+      Welcome to Dasher!</Text>
+
+      <TextInput
+      style={styles.textbox}
+      placeholder = "Username" placeholderTextColor = 'rgba(0,0,0,0.5)'
+      onChangeText = {(text) => setUsername(text)}
       />
+
+      <TextInput
+      secureTextEntry={true}
+      style={styles.textbox}
+      placeholder = "Password" placeholderTextColor = 'rgba(0,0,0,0.5)'
+      />
+
+      <TouchableOpacity
+        //onPress={() => alert(`You are logged in as ${username}`)}
+        onPress={() => navigation.navigate('Main')}
+        style={{ backgroundColor: '#fff' }}>
+        <Text style={styles.button}>Login</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -74,6 +97,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#21a35e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: 'black',
+    fontSize: 30,
+    backgroundColor: 'white',
+    marginHorizontal: 15, 
+    marginVertical: 20,
+    padding: 7,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  }, 
+  button: {
+    color: 'black',
+    fontSize: 20,
+    marginHorizontal: 10,
+    marginVertical: 10, 
+    borderRadius: 5,
+  },
+  textbox: {
+  	backgroundColor: "rgba(0,0,0,0.1)",
+  	height: 40, 
+  	width: 200,
+  	marginTop: 0,
+  	marginBottom: 20,
+  	borderRadius: 5,
+  	borderColor: 'white', 
+  	borderWidth: 1,
+  	padding: 5,
   },
 });
 
