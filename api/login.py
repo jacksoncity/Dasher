e806ab607f09afafdffc528f81edb9e96c184a09
jacksoncity <jacksonstokke@gmail.com>
2021-03-06 18:38:16 -0600

    fixed readability in login.py adn added a logout method

--- api/login.py
+++ api/login.py
@@ -12,20 +12,33 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 app = Flask(__name__)
 Bootstrap(app)
 
+'''
+A flaskfrom that hold all of the credentials of the user to be logged in
+'''
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=5, max=20)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=40)])
     remember = BooleanField('remember me')
 
+'''
+A flaskfrom that hold all of the credentials of the user to be signed up for the app
+'''
 class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=5, max=20)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=40)])
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email'), Length(max=50)])
 
+
 @app.route('/')
 def index():
     return jsonify(data="Landing page so it's not an error")
 
+
+'''
+This method allows the user to login and access the main page of the app should the 
+credentials of the login be valid, otherwise it fails the login and they are returned 
+to a blank login page again.
+'''
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
@@ -35,9 +48,13 @@ def login():
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
-                return redirect(url_for('mainmenu'))
+                return redirect(url_for('mainmenu')) #this needs to return the user to the main page but idk how to do that quite yet lmao
     return "login failed"
 
+'''
+This method is the gernal purpose signup that allows the user to be able to login to the app
+should the input credentials be valid
+'''
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
@@ -46,5 +63,16 @@ def signup():
         new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(new_user)
         db.session.commit()
-        return "new user had been created!"
-    return
+        return "new user had been created!" #then needs to return the user to the login page
+
+    return #needs to return the user to the login page
+
+'''
+On logout this method returns the user to the login page as they cannot access the app if not logged in.
+'''
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
